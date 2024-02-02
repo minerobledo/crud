dbbfbb21e36dadbba1c3201f833c3e7fbcd23d9a
Franco <francorobledo64@gmail.com>
2024-02-02 17:59:20 -0300

    Segundo commit

---
+++ PrimerCRUD/Models/DbcrudContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PrimerCRUD;
+
+namespace PrimerCRUD.Models;
+
+public partial class DbcrudContext : DbContext
+{
+    
+    private readonly IConfiguration _configuration;
+    
+
+    public DbcrudContext(DbContextOptions<DbcrudContext> options,IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+  
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+   
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration["ConnectionString:conexion"]);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuario__3214EC07B7C04E4F");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Fecha)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PrimerCRUD/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimerCRUD.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Clave { get; set; }
+
+    public DateTime? Fecha { get; set; }
+}
